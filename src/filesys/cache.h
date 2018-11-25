d5d036e106b80ee504879c420e79438cc9035668
Qian Wang <qian@Qians-MacBook-Pro.local>
2018-11-25 14:13:01 -0800

    try out using array

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -18,7 +18,7 @@ void new_cache_block(int i_block);
 void cache_init(void);
 void cache_mark_block_dirty(struct cache_block * c_block);
 int cache_get_free_block(void);
-void cache_write_back(int i_block)
+void cache_write_back(int i_block);
 void cache_flush(void);
 // Clock algorithm evicting cache
 void cache_evict(void); 
