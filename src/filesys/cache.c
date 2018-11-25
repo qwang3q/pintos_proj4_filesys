344b0497340c70b4d6a46e444f3d25e20d0c61e1
Qian Wang <qian@Qians-MacBook-Pro.local>
2018-11-25 15:20:37 -0800

    try out using array

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -63,11 +63,8 @@ cache_get_free_block(void) {
 
 void 
 cache_write_back(int i_block) {
-    if(!cache_all_blocks[i_block].dirty)
-        return;
-    
-    // TODO: write back to disk
     block_write(fs_device, cache_all_blocks[i_block].disk_sector, cache_all_blocks[i_block].block);
+    ache_all_blocks[i_block].dirty = false;
 }
 
 void 
@@ -77,7 +74,6 @@ cache_flush(void) {
   for(i_block = 0; i_block < CACHE_CAPACITY; i_block++) {
     if(cache_all_blocks[i_block].dirty == true) {
       cache_write_back(i_block);
-      cache_all_blocks[i_block].dirty = false;
     }
   }
 
