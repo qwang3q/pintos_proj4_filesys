e366d77af48b5ade5b4dd3c2984994c73cf07222
Qian Wang <qian@Qians-MacBook-Pro.local>
2018-11-26 21:40:56 -0800

    unused is needed for inode_disk size assertion

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -18,7 +18,7 @@ struct inode_disk
     block_sector_t start;               /* First data sector. */
     off_t length;                       /* File size in bytes. */
     unsigned magic;                     /* Magic number. */
-    //uint32_t unused[125];               /* Not used. */
+    uint32_t unused[125];               /* Not used. */
   };
 
 /* Returns the number of sectors to allocate for an inode SIZE
