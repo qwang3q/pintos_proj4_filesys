512fff5f354f948fcce20edee4333a740b89e41e
Qian Wang <qian@Qians-MacBook-Pro.local>
2018-12-01 17:59:00 -0800

    IMPORTANT: load block data from sector before converting

--- src/filesys/directory.c
+++ src/filesys/directory.c
@@ -37,7 +37,8 @@ dir_open (struct inode *inode)
   printf("CD- directory::dir_open: inode sector is: %d, ", inode->sector);
   printf("length: %d ", inode->data.length);
   printf("data: ");
-  for(int i = 0; i < 1; i++) {
+  int i;
+  for(i = 0; i < 1; i++) {
     printf("%d ", inode->data.direct_blocks[i]);
   }
   printf("\n");
